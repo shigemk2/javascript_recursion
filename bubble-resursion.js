d1318447d7aa67898cbb22209dd83a5bbe8d803e
shigemk2 <i.am.shige@gmail.com>
2014-11-03 00:28:03 +0900

    再帰バブルソート

--- bubble-resursion.js
+++ bubble-resursion.js
@@ -1,5 +1,5 @@
 function bswap(n, x) {
-  if(n <= 1) return x;
+  if(n <= 0) return x;
   if(x[n] < x[n-1]) {
     var bs = x[n];
     x[n] = x[n-1];
@@ -10,8 +10,10 @@ function bswap(n, x) {
 
 function bubble(x) {
   var n = x.length;
-  if (x.length == 0) return x;
-  return bubble(bswap(n - 1, x.slice(1, x.length)));
+  if (n == 0) return x;
+  bswap(n - 1, x);
+  var x1 = x.shift();
+  return [x1].concat(bubble(x));
 };
 
 console.log(bubble([4, 6, 9, 8, 3, 5, 1, 7, 2]));
