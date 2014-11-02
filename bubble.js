b82bb4c79ebf595f2c55aada55ca8bdbb3238e1b
shigemk2 <i.am.shige@gmail.com>
2014-11-02 23:34:03 +0900

    バブルソート ループ

---
+++ bubble.js
@@ -0,0 +1,14 @@
+function bubble(x) {
+  for(var i=0; i<x.length-1; i++) {
+    for(var j=x.length-1; j>i; j--) {
+      if(x[j] < x[j-1]) {
+        var bswap = x[j];
+        x[j] = x[j-1];
+        x[j-1] = bswap;
+      };
+    };
+  };
+  return x;
+};
+
+console.log(bubble([4, 6, 9, 8, 3, 5, 1, 7, 2]));
