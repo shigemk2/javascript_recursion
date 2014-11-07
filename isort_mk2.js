8b1881cc60fea9bbddea946e58ee58b9f972393f
shigemk2 <i.am.shige@gmail.com>
2014-11-08 02:05:10 +0900

    concatでちょっと修正

--- isort_mk2.js
+++ isort_mk2.js
@@ -1,15 +1,14 @@
-function iswap(x) {
-  return x;
+function insert(x, xs) {
+  if (x) {
+    return x;
+  }
 }
 
 function isort(x) {
-  x.splice(0,1);
-  var x1 = x[0];
-  console.log(x1);
-  if(x.length === 0) {
-    return x1;
-  } else {
-    return isort(x);
-  }
+  var n = x.length;
+  if(n <= 1) return x;
+  var x1 = x.shift(x);
+  var x2 = x.shift(x);
+  return [x1, x2].concat(isort(x));
 }
 console.log(isort([4,3,5,2,1]));
