dcf981aca590cac8561732409d2b89cf1557802e
shigemk2 <i.am.shige@gmail.com>
2014-11-08 23:52:47 +0900

    Fix

--- isort_mk2.js
+++ isort_mk2.js
@@ -1,14 +1,19 @@
 function insert(x, xs) {
-  if (x) {
-    return x;
+  if(xs.length <= 0) {
+    return [x];
+  }
+  var y = xs.shift();
+  if (x < y) {
+    return [x].concat([y].concat(xs));
+  } else {
+    return [y].concat(insert(x, xs));
   }
 }
 
 function isort(x) {
-  var n = x.length;
-  if(n <= 1) return x;
-  var x1 = x.shift(x);
-  var x2 = x.shift(x);
-  return [x1, x2].concat(isort(x));
+  if(x.length <= 0) {
+    return x;
+  }
+  return insert(x.shift(), (isort(x)));
 }
 console.log(isort([4,3,5,2,1]));
